118e7d68087b1b085ef65443bda625b8eca00ede
jpcluff <jpaul.clough@gmail.com>
2024-02-29 20:56:40 +0000

    Initial commit

---
+++ visualising-data/script.js
@@ -0,0 +1,4 @@
+const peoplePets = 
+    "{"petOwner": "Rabindranath", "pet": "Hugh"}, {"petOwner":"Arundhati", "pet":"Pugh"}, {"petOwner":"Amitav", "pet":"Barley MaGrew"}, {"petOwner":"Shashi", "pet":"Cuthbert"}, {"petOwner":"Khushwant", "pet":"Dibble"}, {"petOwner":"Vikram", "pet":"Grub"}";
+let parsedJson = JSON.parse(petOwner);
+console.log(parsedJson);
