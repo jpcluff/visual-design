fc00b4e5ebdfc96f643bfb5d97bb1e315d2259da
jpcluff <jpaul.clough@gmail.com>
2024-03-02 13:13:52 +0000

    Working code

--- visualising-data/script.js
+++ visualising-data/script.js
@@ -1,4 +1,17 @@
-const peoplePets = 
-    "{"petOwner": "Rabindranath", "pet": "Hugh"}, {"petOwner":"Arundhati", "pet":"Pugh"}, {"petOwner":"Amitav", "pet":"Barley MaGrew"}, {"petOwner":"Shashi", "pet":"Cuthbert"}, {"petOwner":"Khushwant", "pet":"Dibble"}, {"petOwner":"Vikram", "pet":"Grub"}";
-let parsedJson = JSON.parse(petOwner);
-console.log(parsedJson);
+
+const myRequest = new Request("peoplePets.json");
+fetch(myRequest)
+    .then(response => {return response.json();
+    })
+    .then(dataObj => {
+        console.log(dataObj);
+        const myList = document.querySelector("ul");
+        let petArray = dataObj.peoplePets;
+        for (const personPet of petArray) {
+            const listItem = document.createElement("li");
+            listItem.appendChild(document.createElement("strong")).textContent = personPet.petOwner;
+            listItem.append(` owns ${personPet.pet}`);
+            myList.appendChild(listItem);
+        }
+    })
+    .catch(console.error);
